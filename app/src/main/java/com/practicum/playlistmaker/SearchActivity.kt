9846e42cff8b15c3a426fb6d150ab2a9641dcdd5
atom327 <svv.vinokurov@mail.ru>
2025-02-06 23:57:48 +0400

    Домашнее задание спринта 11

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -6,16 +6,42 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
     private var editTextValue: String = TEXT_VALUE
+    private val iTunesBaseUrl = "https://itunes.apple.com"
+    private val retrofit =
+        Retrofit.Builder().baseUrl(iTunesBaseUrl).addConverterFactory(GsonConverterFactory.create())
+            .build()
+    private val itunesService = retrofit.create(ITunesAPI::class.java)
+    private val tracks = ArrayList<Track>()
+    private val adapter = TrackListAdapter()
+
+    private lateinit var toolbarButton: Toolbar
+    private lateinit var inputEditText: EditText
+    private lateinit var clearButton: ImageView
+    private lateinit var trackList: RecyclerView
+    private lateinit var placeholderMessage: LinearLayout
+    private lateinit var placeholderImage: ImageView
+    private lateinit var placeholderText: TextView
+    private lateinit var updateButton: Button
 
     companion object {
         const val TEXT_KEY = "TEXT_KEY"
@@ -26,9 +52,21 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val toolbarButton = findViewById<Toolbar>(R.id.toolbarButton)
-        val inputEditText = findViewById<EditText>(R.id.searchEditText)
-        val clearButton = findViewById<ImageView>(R.id.searchClearIcon)
+        toolbarButton = findViewById(R.id.toolbarButton)
+        inputEditText = findViewById(R.id.searchEditText)
+        clearButton = findViewById(R.id.searchClearIcon)
+        trackList = findViewById(R.id.rvTrackList)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        placeholderImage = findViewById(R.id.placeholderImage)
+        placeholderText = findViewById(R.id.placeholderText)
+        updateButton = findViewById(R.id.updateButton)
+
+        updateButton.visibility = View.GONE
+
+        adapter.tracks = tracks
+
+        trackList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        trackList.adapter = adapter
 
         if (savedInstanceState != null) {
             editTextValue = savedInstanceState.getString(TEXT_KEY, TEXT_VALUE)
@@ -50,9 +88,18 @@ class SearchActivity : AppCompatActivity() {
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
 
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+                true
+            }
+            false
+        }
+
         clearButton.setOnClickListener {
             inputEditText.setText("")
             hideSoftKeyboard(it)
+            tracks.clear()
         }
 
         toolbarButton.setNavigationOnClickListener {
@@ -61,40 +108,51 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val trackList = listOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        val recycler = findViewById<RecyclerView>(R.id.rvTrackList)
-        recycler.adapter = TrackListAdapter(trackList)
+        updateButton.setOnClickListener {
+            search()
+        }
+    }
+
+    private fun search() {
+        itunesService.search(inputEditText.text.toString())
+            .enqueue(object : Callback<ITunesResponse> {
+                override fun onResponse(
+                    call: Call<ITunesResponse>, response: Response<ITunesResponse>
+                ) {
+                    if (response.code() == 200) {
+                        tracks.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            tracks.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                            showMessage("")
+                        } else {
+                            showMessage(getString(R.string.nothing_found))
+                        }
+                    } else showMessage(getString(R.string.something_went_wrong))
+                }
+
+                override fun onFailure(call: Call<ITunesResponse>, t: Throwable) {
+                    showMessage(getString(R.string.something_went_wrong))
+                }
+            })
+    }
+
+    private fun showMessage(text: String) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            placeholderText.text = text
+            if (text == getString(R.string.something_went_wrong)) {
+                placeholderImage.setImageResource(R.drawable.something_went_wrong)
+                updateButton.visibility = View.VISIBLE
+            } else {
+                placeholderImage.setImageResource(R.drawable.nothing_found)
+                updateButton.visibility = View.GONE
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
